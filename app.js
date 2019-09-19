38945d93ccdd65dcf03964b52654a62f0944feee
Matthew Gleich <matthewgleich@gmail.com>
2019-09-19 14:34:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ GAME RULES:
 var scores = [0, 0];
 var roundScore = 0;
 var activePlayer = 0;
-var dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
-// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "<em>";
-
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
 
 document.querySelector(".dice").style.display = "none";
+
+function btn() {
+    // Do something here
+}
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+});
